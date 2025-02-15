853b72386549102a827eaef60056cf8a67be8385
Jackson Cheriyan Kollareth <138844080+ChackzWolf@users.noreply.github.com>
2025-02-15 14:51:35 +0530

    Updated : opt route

--- src/components/tutor/auth/register.tsx
+++ src/components/tutor/auth/register.tsx
@@ -105,7 +105,7 @@ function RegisterTutor() {
             localStorage.removeItem('otpCountDown');
             if(response.data.success){
                 dispatch(setTutorData(response.data.tutorData))
-                navigate('/register/tutor/otp',{state: response.data});
+                navigate(ROUTES.tutor.signupOTP,{state: response.data});
                 console.log('success' , response.data)
             }else{
                 console.log(response.data)
